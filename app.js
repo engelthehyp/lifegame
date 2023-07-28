891cfa92d3c7c61d8197b0acae95b501c282def9
engelthehyp <engelthehyp@gmail.com>
2023-07-28 14:47:45 -0400

    Refactoring

--- app.js
+++ app.js
@@ -11,169 +11,156 @@ let allLifeAgeId = 1;
 let totalResourceCount = 0;
 
 for (var r = 0; r <= canvas.width / 10; ++r) {
-  let row = [];
-  for (var c = 0; c <= canvas.height / 10; ++c) {
-    row.push(0);
-  }
-  gridArray.push(row);
+	let row = [];
+	for (var c = 0; c <= canvas.height / 10; ++c) {
+		row.push(0);
+	}
+	gridArray.push(row);
 }
 
 //create life
 function init() {
-  createIntialLife(lifeCount);
-  createFood(foodCount);
+	createIntialLife(lifeCount);
+	createFood(foodCount);
 
-  if (showFOV) {
-    for (var l in lifeArray) {
-      lifeArray[l].drawFOV();
-    }
-  }
-  for (var r in resourceArray) {
-    resourceArray[r].draw();
-  }
-  if (showGrid) {
-    drawGrid();
-    drawDivGrid();
-  }
-  for (var l in lifeArray) {
-    lifeArray[l].draw();
-  }
+	if (showFOV) {
+		for (var l in lifeArray) {
+			lifeArray[l].drawFOV();
+		}
+	}
+	for (var r in resourceArray) {
+		resourceArray[r].draw();
+	}
+	if (showGrid) {
+		drawGrid();
+		drawDivGrid();
+	}
+	for (var l in lifeArray) {
+		lifeArray[l].draw();
+	}
 }
 
 window.addEventListener("keydown", handler);
 window.addEventListener("mousedown", getInfo);
 
 function changeFOV() {
-  showFOV = !showFOV;
+	showFOV = !showFOV;
 }
 
 function changeGrid() {
-  showGrid = !showGrid;
+	showGrid = !showGrid;
 }
 
 function handler(e) {
-  if (e.key === "f") {
-    changeFOV();
-  } else if (e.key === "g") {
-    changeGrid();
-  }
+	void {
+		"f": changeFOV,
+		"g": changeGrid,
+	}[e.key]?.();
 }
 
-setInterval(() => {
-  update();
-}, 100);
+setInterval(update, 100);
 
 function getInfo(e) {
-  console.log(e);
-  var rect = e.target.getBoundingClientRect();
-  let x = e.clientX - rect.left;
-  let y = e.clientY - rect.top;
-  x = Math.floor(x / 10);
-  y = Math.floor(y / 10);
-  console.log(x, y);
+	console.log(e);
+	var rect = e.target.getBoundingClientRect();
+	let x = e.clientX - rect.left;
+	let y = e.clientY - rect.top;
+	x = Math.floor(x / 10);
+	y = Math.floor(y / 10);
+	console.log(x, y);
 
-  for (var l = 0; l < lifeArray.length; ++l) {
-    let lf = lifeArray[l];
-    if (lf.x === x && lf.y === y) {
-      lf.selected = true;
-    } else {
-      lf.selected = false;
-    }
-  }
+	lifeArray.forEach(life => life.selected = life.x === x && life.y === y);
 }
 
 function update(e) {
-  // console.log(lifeArray.length);
-  //console.log(e);
+	// console.log(lifeArray.length);
+	//console.log(e);
 
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  for (var l in lifeArray) {
-    let lf = lifeArray[l];
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	for (var l in lifeArray) {
+		let lf = lifeArray[l];
 
-    lifeArray[l].move();
+		lifeArray[l].move();
 
-    if (lifeArray[l].stats.health <= 0) {
-      let dead = lifeArray[l];
-      gridArray[dead.y][dead.x] = 0;
-      lifeArray.splice(l, 1);
-    }
-  }
-  if (showFOV) {
-    for (var l in lifeArray) {
-      lifeArray[l].drawFOV();
-    }
-  }
-  for (var r in resourceArray) {
-    resourceArray[r].draw();
-  }
+		if (lifeArray[l].stats.health <= 0) {
+			let dead = lifeArray[l];
+			gridArray[dead.y][dead.x] = 0;
+			lifeArray.splice(l, 1);
+		}
+	}
+	if (showFOV) {
+		for (var l in lifeArray) {
+			lifeArray[l].drawFOV();
+		}
+	}
+	for (var r in resourceArray) {
+		resourceArray[r].draw();
+	}
 
-  if (showGrid) {
-    drawGrid();
-    //drawDivGrid();
-  }
-  for (var l in lifeArray) {
-    lifeArray[l].draw();
-  }
+	if (showGrid) {
+		drawGrid();
+		//drawDivGrid();
+	}
+	for (var l in lifeArray) {
+		lifeArray[l].draw();
+	}
 
-  updateStatGui();
-  turnCount++;
-  totalTurnCount++;
-  if (turnCount >= 50) {
-    createFood(50);
-    turnCount = 0;
-  }
+	updateStatGui();
+	if (++totalTurnCount, ++turnCount >= 50) createFood(50 + (turnCount = 0)); // Channeling C
 }
 
 function createIntialLife(number, x, y) {
-  if (number >= 3) {
-    for (var i = 0; i < number; ++i) {
-      // let team = i < number / 2 ? "red" : "blue";
+	if (number >= 3) {
+		for (var i = 0; i < number; ++i) {
+			// let team = i < number / 2 ? "red" : "blue";
 
-      let team =
-        "rgb(" +
-        Math.floor(Math.random() * 255) +
-        "," +
-        Math.floor(Math.random() * 255) +
-        "," +
-        Math.floor(Math.random() * 255) +
-        ")";
+			let team =
+				"rgb(" +
+				Math.floor(Math.random() * 255) +
+				"," +
+				Math.floor(Math.random() * 255) +
+				"," +
+				Math.floor(Math.random() * 255) +
+				")";
 
-      let l = new Life(i, i, allLifeAgeId, team);
-      lifeArray.push(l);
-      try {
-        gridArray[l.x][l.y] = l.id;
-      } catch (e) {
-        console.log(e);
-        console.log(gridArray, l);
-      }
-      allLifeAgeId++;
-    }
-  } else {
-    let l = new Life(10, 10, 1, "blue");
-    lifeArray.push(l);
-    gridArray[l.x][l.y] = l.id;
-  }
+			let l = new Life(i, i, allLifeAgeId, team);
+			lifeArray.push(l);
+			try {
+				gridArray[l.x][l.y] = l.id;
+			} catch (e) {
+				console.log(e);
+				console.log(gridArray, l);
+			}
+			allLifeAgeId++;
+		}
+	} else {
+		let l = new Life(10, 10, 1, "blue");
+		lifeArray.push(l);
+		gridArray[l.x][l.y] = l.id;
+	}
 }
 
 function createFood(number) {
-  let count = 0;
-  while (count <= number) {
-    let x = Math.floor((Math.random() * canvas.width) / 10);
-    let y = Math.floor((Math.random() * canvas.height) / 10);
-    //let x = 11;
-    //let y = 8;
+	const dimension = d => Math.floor((Math.random() * d) / 10);
+	let count = 0;
 
-    let gridSpot = gridArray[y][x];
-    if (gridSpot === 0) {
-      let food = new Resource(x, y, "food", totalResourceCount);
-      //console.log(food.id);
-      resourceArray.push(food);
-      gridArray[y][x] = food.id;
-      totalResourceCount++;
-    }
+	while (count <= number) {
+		let x = dimension(canvas.width);
+		let y = dimension(canvas.height);
+		//let x = 11;
+		//let y = 8;
 
-    count++;
-  }
+		let gridSpot = gridArray[y][x];
+		if (gridSpot === 0) {
+			let food = new Resource(x, y, "food", totalResourceCount);
+			//console.log(food.id);
+			resourceArray.push(food);
+			gridArray[y][x] = food.id;
+			totalResourceCount++;
+		}
+
+		count++;
+	}
 }
 
 init();
